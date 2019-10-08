5b939a568ed25df4d90f166196b345b84bce9656
unknown <panda_gomdol@naver.com>
2019-10-08 12:38:25 +0900

    Laracasts - Laravel 6 From Scratch ~13

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*
+Route::get('/posts/{post}', function ($post) {
+    $posts = [
+        'my-first-post' => 'Hello World!',
+        'my-second-post' => 'test'
+    ];
+
+    if (!array_key_exists($post, $posts)) {
+        abort(404, 'Sorry, that post was not found.');
+    }
+
+    return view('post', [
+        'post' => $posts[$post]
+    ]);
+});
+*/
+
+Route::get('/posts/{post}', 'PostsController@show');
