63e82cc6c08966b9f233c71f33f81f3d93731927
unknown <panda_gomdol@naver.com>
2019-10-15 13:02:09 +0900

    Laracasts - Laravel 6 From Scratch 23~24

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -26,22 +26,41 @@ class ArticlesController extends Controller
 
     public function create()
     {
-        // Shows a view to create a new resource
+        return view('articles.create');
     }
 
     public function store()
     {
-        // Persist the new resource
+        // validation
+
+        $article = new Article();
+        $article->title = request('title');
+        $article->excerpt = request('excerpt');
+        $article->body = request('body');
+
+        $article->save();
+        
+        return redirect('/articles');
     }
 
-    public function edit()
+    public function edit($id)
     {
-        // Show a view to edit an existing resource
+        $article = Article::find($id);
+
+        return view('articles.edit', compact('article'));
     }
 
     public function update()
     {
-        // Persist the edited resource
+        $article = Article::find($id);
+
+        $article->title = request('title');
+        $article->excerpt = request('excerpt');
+        $article->body = request('body');
+
+        $article->save();
+        
+        return redirect('/articles' . $article->id);
     }
 
     public function destroy()
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -21,6 +21,8 @@ Released   : 20140225
 <link href="/css/fonts.css" rel="stylesheet" type="text/css" media="all" />
 <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css" media="all" />
 
+@yield('head')
+
 </head>
 <body>
     <div id="header-wrapper">
